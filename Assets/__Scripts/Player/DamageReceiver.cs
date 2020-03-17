6986e4b90972a9f3592616969e2017235529410a
chrisowski1 <35178770+chrisowski1@users.noreply.github.com>
2020-03-17 19:31:18 -0400

    [Player] added player health
    Issue #4

---
+++ Assets/__Scripts/Player/DamageReceiver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageReceiver : MonoBehaviour, IEntity
+{
+    public float playerHP = 100;
+    public Text DeathText;    
+
+    public void ApplyDamage(float dmg)
+    {
+        playerHP -= dmg;
+
+        if(playerHP <= 0)
+        {
+            playerHP = 0;
+            DeathText.text = "You Died";
+            StartCoroutine(ExecuteAfterTime(5)); // waits 5 seconds, then runs ExecuteAfterTime function
+        }
+    }
+
+    IEnumerator ExecuteAfterTime(float time){ 
+        yield return new WaitForSeconds(time); // waits for time seconds
+        Application.LoadLevel(0); // reloads the level
+    }
+}
