0102f7e22563013cc659b97b9198fb8ef7d9a20b
chrisowski1 <35178770+chrisowski1@users.noreply.github.com>
2020-03-19 20:50:29 -0400

    [Environment] added win condition
    Issue #5
    +made it so that the user wins the level when they reach the final platform

---
+++ Assets/__Scripts/Player/WinParkour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinParkour : MonoBehaviour
+{
+    public Transform groundTest;
+    public float groundDist = 0.7f; //radius of ground check
+    public LayerMask winMask;
+    public Text winText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Physics.CheckSphere(groundTest.position, groundDist, winMask)){
+            winText.text = "You Win!";            
+            Time.timeScale = 0f; //stops time so player cannot move
+        }
+        
+    }
+}
