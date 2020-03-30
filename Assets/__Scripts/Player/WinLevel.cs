26fdf8a889b1524d6f249938e2d5c7438ee229c2
michaelTaran <60658626+michaelTaran@users.noreply.github.com>
2020-03-30 13:22:02 -0400

    Updated Level Transitions
    Merged winparkour and winmaze scripts

--- Assets/__Scripts/Player/WinLevel.cs
+++ Assets/__Scripts/Player/WinLevel.cs
@@ -2,19 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class WinParkour : MonoBehaviour
+public class WinLevel : MonoBehaviour
 {
     public Transform groundTest;
     public float groundDist = 0.7f; //radius of ground check
     public LayerMask winMask;
     public Text winText;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -23,7 +18,7 @@ public class WinParkour : MonoBehaviour
             winText.text = "Level Complete!\nPress Enter to continue to the next level";          
             Time.timeScale = 0f; //stops time so player cannot move
             if(Input.GetKeyDown(KeyCode.Return)){
-                Application.LoadLevel(2);
+                Application.LoadLevel((SceneManager.GetActiveScene().buildIndex)+1);
             }
         }
         
