9c88405a408663de9e2bc94952f7cbcc4f351a1d
Tony Nguyen <35637486+t-v-nguyen@users.noreply.github.com>
2020-04-01 05:53:09 -0400

    [Player] Added script to toggle the skill point UI
    issue #7 : When the equal sign key is pressed, it will toggle the skill point UI. The cursor will also be toggled.

--- Assets/__Scripts/SkillPointMenu.cs
+++ Assets/__Scripts/SkillPointMenu.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class SkillPointMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static bool gameIsPaused = false;
+    public GameObject skillPointUI;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Equals))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        skillPointUI.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        gameIsPaused = false;
+    }
+    void Pause()
+    {
+        skillPointUI.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        gameIsPaused = true;
         
     }
 }
