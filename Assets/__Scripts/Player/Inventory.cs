b42eb3007c08902c1208ab7c1a764b2fe1d0233e
chrisowski1 <35178770+chrisowski1@users.noreply.github.com>
2020-04-01 18:38:02 -0400

    [Player] Added inventory UI and weapon pickups (WIP)
    Issue #7

--- Assets/__Scripts/Player/Inventory.cs
+++ Assets/__Scripts/Player/Inventory.cs
@@ -11,11 +11,28 @@ public class Inventory : MonoBehaviour
     public Camera playerCamera; // player's camera
     public Weapon defaultPistol; // default weapon the player always has
     public Weapon firstWeapon; // first weapon slot
-    public Weapon secondWeapon; // second weapon slot
-    public Weapon bonusWeapon; // bonus weapon to show differences in all ammo types //TODO
+    public Weapon secondWeapon; // second weapon slot    
     public Text ammoCount; // ammo remaining display value
     public Text reloadWarning; // warns player to reload their gun
     public AudioClip swapAudio; // sound of switching weapons
+
+    public Image slot1;
+    public Image slot2;
+    public Image slot3;
+    public Image slotHealth;
+    Color green = new Color(0f, 1f, 0f, 0.3f);
+    Color def = new Color(0.5f, 0.5f, 0.5f, 0.3f);
+
+    public Weapon empSMG;
+    public Weapon plasmaSMG;
+    public Weapon railSMG;    
+    public Weapon empAR;
+    public Weapon plasmaAR;
+    public Weapon railAR;
+    public Weapon empSR;
+    public Weapon plasmaSR;
+    public Weapon railSR;
+    
 
     [HideInInspector]
     public Weapon currentWeapon; // weapon the player is currently holding
@@ -24,25 +41,59 @@ public class Inventory : MonoBehaviour
 
     // Start is called before the first frame update
     void Start() {
-    
+        //sets first weapons
+        firstWeapon = empSMG;
+        secondWeapon = plasmaAR;        
+
+        // set all weapons to false
+        empSMG.ActivateWeapon(false);
+        plasmaSMG.ActivateWeapon(false);
+        railSMG.ActivateWeapon(false);    
+        empAR.ActivateWeapon(false);
+        plasmaAR.ActivateWeapon(false);
+        railAR.ActivateWeapon(false);
+        empSR.ActivateWeapon(false);
+        plasmaSR.ActivateWeapon(false);
+        railSR.ActivateWeapon(false); 
+
         // pistol is open by default
         defaultPistol.ActivateWeapon(true); // activates the pistol
         firstWeapon.ActivateWeapon(false); // 1st slot disabled
         secondWeapon.ActivateWeapon(false); // 2nd slot disabled
-        currentWeapon = defaultPistol; // player has pistol in hand by default
-
-        bonusWeapon.ActivateWeapon(false);
+        currentWeapon = defaultPistol; // player has pistol in hand by default       
         
         // setting this file to the manager for all the weapon slots
         defaultPistol.manager = this; 
         firstWeapon.manager = this;
-        secondWeapon.manager = this;
-
-        bonusWeapon.manager = this;
+        secondWeapon.manager = this;        
 
         _source = GetComponent<AudioSource>(); // gets the audio
         _source.playOnAwake = false; // does not play on startup
         _source.spatialBlend = 1f; // makes the sound 3D
+
+        ChangeColor(slot1, green);
+        ChangeColor(slot2, def);
+        ChangeColor(slot3, def);
+        ChangeColor(slotHealth, def);
+    }
+
+    // runs when player is colliding with pickup
+    void OnTriggerEnter(Collider other){ // runs when the player collides 
+        if(other.gameObject.CompareTag("railSR")){ 
+            if(firstWeapon == currentWeapon){
+                firstWeapon.ActivateWeapon(false);
+                firstWeapon = railSR;
+                firstWeapon.ActivateWeapon(true);
+            }else if(secondWeapon == currentWeapon){
+                secondWeapon.ActivateWeapon(false);
+                secondWeapon = railSR;
+                secondWeapon.ActivateWeapon(true);
+            }                
+        }   
+    }
+    public void ChangeColor (Image img, Color col)
+    {
+        img.color = col;
     }
 
     // Update is called once per frame
@@ -53,13 +104,16 @@ public class Inventory : MonoBehaviour
 
             defaultPistol.ActivateWeapon(true); // switch to pistol
             firstWeapon.ActivateWeapon(false); // 1st slot disabled
-            secondWeapon.ActivateWeapon(false); // 2nd slot disabled
-            currentWeapon = defaultPistol; // player now has pistol in hand
-
-            bonusWeapon.ActivateWeapon(false);
+            secondWeapon.ActivateWeapon(false); // 2nd slot disabled            
+            currentWeapon = defaultPistol; // player now has pistol in hand            
 
             _source.clip = swapAudio; // sets swap audio
             _source.Play(); // plays swap audio
+            
+            ChangeColor(slot1, green);
+            ChangeColor(slot2, def);
+            ChangeColor(slot3, def);
+            ChangeColor(slotHealth, def);
         }
         
         // brings up first weapon when pressing 2
@@ -67,13 +121,16 @@ public class Inventory : MonoBehaviour
 
             defaultPistol.ActivateWeapon(false); // pistol disabled
             firstWeapon.ActivateWeapon(true); // switch to 1st slot
-            secondWeapon.ActivateWeapon(false); // 2nd slot disabled
-            currentWeapon = firstWeapon; // player now has weapon 1 in hand
-
-            bonusWeapon.ActivateWeapon(false);
+            secondWeapon.ActivateWeapon(false); // 2nd slot disabled            
+            currentWeapon = firstWeapon; // player now has weapon 1 in hand            
             
             _source.clip = swapAudio; // sets swap audio
             _source.Play(); // plays swap audio
+
+            ChangeColor(slot1, def);
+            ChangeColor(slot2, green);
+            ChangeColor(slot3, def);
+            ChangeColor(slotHealth, def);
         }
 
         // brings up second weapon when pressing 3
@@ -81,27 +138,21 @@ public class Inventory : MonoBehaviour
 
             defaultPistol.ActivateWeapon(false); // pistol disabled
             firstWeapon.ActivateWeapon(false); // 1st slot disabled
-            secondWeapon.ActivateWeapon(true); // switch to 2nd slot
-            currentWeapon = secondWeapon; // player now has weapon 2 in hand
-
-            bonusWeapon.ActivateWeapon(false);
+            secondWeapon.ActivateWeapon(true); // switch to 2nd slot            
+            currentWeapon = secondWeapon; // player now has weapon 2 in hand            
             
             _source.clip = swapAudio; // sets swap audio
             _source.Play(); // plays swap audio
+
+            ChangeColor(slot1, def);
+            ChangeColor(slot2, def);
+            ChangeColor(slot3, green);
+            ChangeColor(slotHealth, def);
         } 
 
         // brings up second weapon when pressing 3
         if (Input.GetKeyDown(KeyCode.Alpha4)) {
-
-            defaultPistol.ActivateWeapon(false); // pistol disabled
-            firstWeapon.ActivateWeapon(false); // 1st slot disabled
-            secondWeapon.ActivateWeapon(false); // switch to 2nd slot
-            currentWeapon = bonusWeapon; // player now has weapon 2 in hand
-
-            bonusWeapon.ActivateWeapon(true);
             
-            _source.clip = swapAudio; // sets swap audio
-            _source.Play(); // plays swap audio
         }
 
         // display the current ammo and the reserve ammo
