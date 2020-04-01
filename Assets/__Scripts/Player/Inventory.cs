11d4ef9474d5142072e47551326ca587d66e8a28
Aiden <12aduffy@gmail.com>
2020-04-01 15:33:20 -0400

    [FinalLevel] Added Boss animation
    Issue #7
    
    + Added final boss summon animation
    + set up skeleton of Boss AI

--- Assets/__Scripts/Player/Inventory.cs
+++ Assets/__Scripts/Player/Inventory.cs
@@ -12,7 +12,7 @@ public class Inventory : MonoBehaviour
     public Weapon defaultPistol; // default weapon the player always has
     public Weapon firstWeapon; // first weapon slot
     public Weapon secondWeapon; // second weapon slot
-    public Weapon bonusWeapon; // bonus weapon to show differences in all ammo types //TODO
+    //public Weapon bonusWeapon; // bonus weapon to show differences in all ammo types //TODO
     public Text ammoCount; // ammo remaining display value
     public Text reloadWarning; // warns player to reload their gun
     public AudioClip swapAudio; // sound of switching weapons
@@ -31,14 +31,14 @@ public class Inventory : MonoBehaviour
         secondWeapon.ActivateWeapon(false); // 2nd slot disabled
         currentWeapon = defaultPistol; // player has pistol in hand by default
 
-        bonusWeapon.ActivateWeapon(false);
+        //bonusWeapon.ActivateWeapon(false);
         
         // setting this file to the manager for all the weapon slots
         defaultPistol.manager = this; 
         firstWeapon.manager = this;
         secondWeapon.manager = this;
 
-        bonusWeapon.manager = this;
+        //bonusWeapon.manager = this;
 
         _source = GetComponent<AudioSource>(); // gets the audio
         _source.playOnAwake = false; // does not play on startup
@@ -56,7 +56,7 @@ public class Inventory : MonoBehaviour
             secondWeapon.ActivateWeapon(false); // 2nd slot disabled
             currentWeapon = defaultPistol; // player now has pistol in hand
 
-            bonusWeapon.ActivateWeapon(false);
+            //bonusWeapon.ActivateWeapon(false);
 
             _source.clip = swapAudio; // sets swap audio
             _source.Play(); // plays swap audio
@@ -70,7 +70,7 @@ public class Inventory : MonoBehaviour
             secondWeapon.ActivateWeapon(false); // 2nd slot disabled
             currentWeapon = firstWeapon; // player now has weapon 1 in hand
 
-            bonusWeapon.ActivateWeapon(false);
+            //bonusWeapon.ActivateWeapon(false);
             
             _source.clip = swapAudio; // sets swap audio
             _source.Play(); // plays swap audio
@@ -84,12 +84,12 @@ public class Inventory : MonoBehaviour
             secondWeapon.ActivateWeapon(true); // switch to 2nd slot
             currentWeapon = secondWeapon; // player now has weapon 2 in hand
 
-            bonusWeapon.ActivateWeapon(false);
+            //bonusWeapon.ActivateWeapon(false);
             
             _source.clip = swapAudio; // sets swap audio
             _source.Play(); // plays swap audio
         } 
-
+    /*
         // brings up second weapon when pressing 3
         if (Input.GetKeyDown(KeyCode.Alpha4)) {
 
@@ -102,7 +102,7 @@ public class Inventory : MonoBehaviour
             
             _source.clip = swapAudio; // sets swap audio
             _source.Play(); // plays swap audio
-        }
+        } */
 
         // display the current ammo and the reserve ammo
         ammoCount.text = currentWeapon.GetAmmo().ToString() + " / " + currentWeapon.magSize.ToString();
