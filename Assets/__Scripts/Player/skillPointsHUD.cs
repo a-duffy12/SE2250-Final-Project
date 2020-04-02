b18cb9c4b6450a76fb0af8c563906b5931ea4c13
Tony Nguyen <35637486+t-v-nguyen@users.noreply.github.com>
2020-04-02 03:39:51 -0400

    [Player] Added HUD for available skill points
    issue #7: A HUD will popup when player has unused skill points.

---
+++ Assets/__Scripts/Player/skillPointsHUD.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skillPointsHUD : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // displays the HUD to notify player if they have available skill points
+        if (PlayerSkillManager.availSkillPoints > 0)
+        {
+            if (SkillPointMenu.gameIsPaused) // if game is paused then the HUD disappears
+            {
+                gameObject.SetActive(false);
+            }
+            else// if game is not paused, the HUD will notify player of available skill points
+            {
+                gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
